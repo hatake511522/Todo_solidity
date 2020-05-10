ea010466c9217def55790c6f93a3ede2afed4cca
hatake511522 <623dgk511522@gmail.com>
2020-05-11 01:04:04 +0900

    Change README

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # はじめに
-React + Solidity のTodoアプリです
+React + Solidity のTodoアプリです.
+chrome に MetaMask をインストールし ropstenテストネットワークからテスト用のETHをもらっておいてください。
+1ETHあれば十分です。
+
 
 #　動作させるには
 
-- [http://remix.ethereum.org/](remix) を立ち上げ Todo.sol を作成しデプロイ
-- MetaMask を起動しておく
-- Todoを作成、アーカイブすると MetaMask を通じてトランザクションが処理されていることを確認する
+1. [http://remix.ethereum.org/](remix) を起動
+2. new fileから Todo.sol を作成しこのフォルダの Todo.sol のコードを貼り付けコンパイル
+3. remixの environment を `Injected Web3` に設定しデプロイ
+4. 数分ほどしたらデプロイが終わるので Deployed Contracts から transaction のアドレスをコピー
+5. user_ABI.jsの address に貼り付ける
+6. `npm start`
+7. Todoを作成、アーカイブすると MetaMask を通じてトランザクションが処理されていることを確認する
+
 
--- Todo.sol
+++ Todo.sol
@@ -1,8 +1,12 @@
+// ⓪ コンパイラバージョンを指定する
 pragma solidity ^0.5.0;
 
+// ① Contract名を宣言
 contract TodoApp {
 
     uint num = 0;
+
+    // ③ 構造体の性質を持った配列を変数todosとして定義
     struct Todo {
         uint taskid;
         string task;
@@ -11,20 +15,28 @@ contract TodoApp {
 
     Todo[] public todos;
 
+    // ④ mappingを宣言
     mapping (uint => address) public todoToOwner;
+
+    // ⑤ mappingを宣言
     mapping (address => uint) public ownerTodoCount;
 
+    // ⑥ Todoを作成する関数 （Gas発生）
     function TodoCreate(string memory _task) public {
         uint id = todos.push( Todo(num, _task, true) ) - 1;
         todoToOwner[id] = msg.sender;
         ownerTodoCount[msg.sender]++;
         num++;
     }
+
+    // ⑦ Todoの状態を完了にする関数 （Gas発生）
     function TodoRemove(uint id) external {
         require(todoToOwner[id] == msg.sender);
         require(todos[id].flag);
         todos[id].flag = false;
     }
+
+    // ⑧ 自分のイーサリアムアドレスに紐づくTodoのidを取得する関数 （ガス代不要）
   	function getTodosByOwner(address owner) external view returns(uint[] memory) {
     	uint[] memory result = new uint[](ownerTodoCount[owner]);
     	uint counter = 0;
@@ -37,4 +49,4 @@ contract TodoApp {
     	return result;
 	}
 
-}
+}u
